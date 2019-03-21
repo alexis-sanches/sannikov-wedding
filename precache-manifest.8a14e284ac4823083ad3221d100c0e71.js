824e8f1e5a49c4e95d014972f46291c0593c2eb0
Aleksandr Sannikov <alexander.v.sannikov@gmail.com>
2019-03-21 08:01:09 +0300

    123

---
+++ precache-manifest.8a14e284ac4823083ad3221d100c0e71.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ac8be448904bcf90cb68ac1fe8064a3",
+    "url": "/sannikov-wedding/static/media/11.6ac8be44.jpeg"
+  },
+  {
+    "revision": "3bcf913835e2afbb8c69",
+    "url": "/sannikov-wedding/static/css/main.d80d8a8b.chunk.css"
+  },
+  {
+    "revision": "86d4e133d78c1620c08b",
+    "url": "/sannikov-wedding/static/js/runtime~main.86d4e133.js"
+  },
+  {
+    "revision": "31ff8effbeef62391df7",
+    "url": "/sannikov-wedding/static/js/2.31ff8eff.chunk.js"
+  },
+  {
+    "revision": "8df967524597cb4ab5d0780121fa6cea",
+    "url": "/sannikov-wedding/static/media/1.8df96752.jpeg"
+  },
+  {
+    "revision": "96cc4242f8bd2013c8e16246a0abb032",
+    "url": "/sannikov-wedding/static/media/2.96cc4242.jpeg"
+  },
+  {
+    "revision": "97d3e04c0667de69ba7b1b32364fabd0",
+    "url": "/sannikov-wedding/static/media/3.97d3e04c.jpeg"
+  },
+  {
+    "revision": "4624efc2d10bf6ff6310590e3c722524",
+    "url": "/sannikov-wedding/static/media/4.4624efc2.jpeg"
+  },
+  {
+    "revision": "f4ca5eb947d97053cf9445939a7742aa",
+    "url": "/sannikov-wedding/static/media/5.f4ca5eb9.jpeg"
+  },
+  {
+    "revision": "beb88deb9cb849747f5e1cb222f8a854",
+    "url": "/sannikov-wedding/static/media/6.beb88deb.jpeg"
+  },
+  {
+    "revision": "d03702604d8d27e5babf69cf8f7ae9d8",
+    "url": "/sannikov-wedding/static/media/7.d0370260.jpeg"
+  },
+  {
+    "revision": "4967d5e549f7f5ae208f35883a0399da",
+    "url": "/sannikov-wedding/static/media/8.4967d5e5.jpeg"
+  },
+  {
+    "revision": "4c660ec6320559a420085052029a8e7d",
+    "url": "/sannikov-wedding/static/media/9.4c660ec6.jpeg"
+  },
+  {
+    "revision": "838858bfe1f60c43f7c0c7d71085480f",
+    "url": "/sannikov-wedding/static/media/10.838858bf.jpeg"
+  },
+  {
+    "revision": "3bcf913835e2afbb8c69",
+    "url": "/sannikov-wedding/static/js/main.3bcf9138.chunk.js"
+  },
+  {
+    "revision": "b1e483320901340f457305950139e154",
+    "url": "/sannikov-wedding/static/media/sweatshirt.b1e48332.svg"
+  },
+  {
+    "revision": "a4b07a411939e79bb4708dde369561c6",
+    "url": "/sannikov-wedding/static/media/calendar.a4b07a41.svg"
+  },
+  {
+    "revision": "b92a197a57ead17598b52604a5c129df",
+    "url": "/sannikov-wedding/static/media/champagne-glass.b92a197a.svg"
+  },
+  {
+    "revision": "baf6a20d2b966b18601b4db0e6553cd1",
+    "url": "/sannikov-wedding/static/media/engagement.baf6a20d.svg"
+  },
+  {
+    "revision": "498685a292428c9a11f7466c77bd5243",
+    "url": "/sannikov-wedding/static/media/clock.498685a2.svg"
+  },
+  {
+    "revision": "03ea0e774786fc69f7c5f0333f21ff08",
+    "url": "/sannikov-wedding/static/media/para1.03ea0e77.jpg"
+  },
+  {
+    "revision": "d2788d34d513152ffc559c5bf75b7938",
+    "url": "/sannikov-wedding/static/media/para2.d2788d34.jpg"
+  },
+  {
+    "revision": "8548ab377aaf82830a41d484c555f934",
+    "url": "/sannikov-wedding/static/media/para3.8548ab37.jpg"
+  },
+  {
+    "revision": "77277ab35e95a6b04a48687a5814e4a3",
+    "url": "/sannikov-wedding/static/media/para4.77277ab3.jpg"
+  },
+  {
+    "revision": "c7cdc8c2b85aab13d9c98d80c7225eee",
+    "url": "/sannikov-wedding/static/media/london.c7cdc8c2.svg"
+  },
+  {
+    "revision": "0823af748b31d23cded1328dcc392ec1",
+    "url": "/sannikov-wedding/static/media/barcelona.0823af74.svg"
+  },
+  {
+    "revision": "ebec6839871c5448cdadfc055561a12a",
+    "url": "/sannikov-wedding/static/media/bg.ebec6839.jpg"
+  },
+  {
+    "revision": "ef18fbebd2ff4bc9281d5c039f88f94e",
+    "url": "/sannikov-wedding/index.html"
+  }
+];
